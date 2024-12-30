38f6f53be86c6d8f88df2823e031ac2b7538ec62
MMahesa <mmahesaa1124@gmail.com>
2024-12-30 21:28:12 +0700

    update main.py & halaman_guru.py

--- UAS/main.py
+++ UAS/main.py
@@ -1,49 +1,47 @@
 import os
 from time import sleep
 
-#import halaman
+# Import halaman
 from halaman_siswa import menu_siswa
 from halaman_guru import menu_guru
 from halaman_nilai import menu_nilai
 from halaman_rapor_siswa import menu_rapor
 
+# Kode untuk warna
+def warna_teks(teks, kode_warna):
+    return f"\033[{kode_warna}m{teks}\033[0m"
 
-# Perintah untuk membersihkan layar 
+# Fungsi untuk membersihkan layar 
 def bersihkan_layar():
     if os.name == "nt":
         os.system("cls")
     else:
         os.system("clear")
 
-
+# Fungsi banner
 def banner():
-    print("\033[1;36m")  # cyan
-    print("=" * 66)
-    print("||                 SELAMAT DATANG DI SISTEM                     ||")
-    print("||               APLIKASI PENGOLAHAN DATA SISWA                 ||")
-    print("||                                                              ||")
-    print("||   dibuat oleh:                                               ||")
-    print("||   [1]Alejandro Wirawan        [4]M Mahesa                    ||")
-    print("||   [2]Dion Enrico Aritonang    [5]Ratih Nawang Wulan          ||")
-    print("||   [3]Holip Patullah                                          ||")
-    print("=" * 66)
-    print("\033[0m")  # reset
+    print(warna_teks("=" * 66, '1;36'))  
+    print(warna_teks("||                 SELAMAT DATANG DI SISTEM                     ||", '1;36'))
+    print(warna_teks("||               APLIKASI PENGOLAHAN DATA SISWA                 ||", '1;36'))
+    print(warna_teks("||                                                              ||", '1;36'))
+    print(warna_teks("||   dibuat oleh:                                               ||", '1;36'))
+    print(warna_teks("||   [1]Alejandro Wirawan        [4]M Mahesa                    ||", '1;36'))
+    print(warna_teks("||   [2]Dion Enrico Aritonang    [5]Ratih Nawang Wulan          ||", '1;36'))
+    print(warna_teks("||   [3]Holip Patullah                                          ||", '1;36'))
+    print(warna_teks("=" * 66, '1;36'))  
 
-
+# Fungsi untuk menampilkan menu utama
 def tampilan_menu():
-    print("\033[1;33m")  # kuning
-    print("|----------------------------------------------------|")
-    print("|                      MENU UTAMA                    |")
-    print("|----------------------------------------------------|")
-    print("| [1] Halaman                                        |")
-    print("| [2] Halaman Guru                                   |")
-    print("| [3] Halaman Penilaian                              |")
-    print("| [4] Halaman Rapor Siswa                            |")
-    print("| [0] Keluar                                         |")
-    print("|----------------------------------------------------|")
-    print("\033[0m") #reset
+    print(warna_teks("|----------------------------------------------------|", '1;33'))  
+    print(warna_teks("|----------------------------------------------------|", '1;33'))  
+    print(warna_teks("| [1] Halaman Siswa                                  |", '1;32')) 
+    print(warna_teks("| [2] Halaman Guru                                   |", '1;34')) 
+    print(warna_teks("| [3] Halaman Penilaian                              |", '1;36'))  
+    print(warna_teks("| [4] Halaman Rapor Siswa                            |", '1;35')) 
+    print(warna_teks("| [0] Keluar                                         |", '1;31'))  
+    print(warna_teks("|----------------------------------------------------|", '1;33'))  
 
-
+# Fungsi untuk animasi loading
 def animasi_load():
     print("\nMemuat", end="")
     titik = 3
@@ -53,14 +51,13 @@ def animasi_load():
         sleep(jeda)
     print("\n")
 
-
 def main():
     while True:
         bersihkan_layar()
         banner()
         tampilan_menu()
         try:
-            pilih = input("\033[1;32mSilahkan Pilih Menu 0-4]: \033[0m")
+            pilih = input(warna_teks("Silahkan Pilih Menu 0-4]: ", '1;32'))  
 
             if pilih in ["0", "1", "2", "3", "4"]:
                 animasi_load()
@@ -74,16 +71,14 @@ def main():
                     menu_rapor()
                 elif pilih == "0":
                     bersihkan_layar()
-                    print(
-                        "\033[1;35mTerima kasih sudah menggunakan sistem ini !\033[0m"
-                    )
+                    print(warna_teks("Terima kasih sudah menggunakan sistem ini!", '1;35'))  
                     sleep(2)
                     break
             else:
-                print("\033[1;31mPilihan tidak valid! Silahkan pilih menu 0-4!\033[0m")
+                print(warna_teks("Pilihan tidak valid! Silahkan pilih menu 0-4!", '1;31'))
                 sleep(2)
         except ValueError:
-            print("\033[1;31mInput harus berupa angka!\033[0m")
+            print(warna_teks("Input harus berupa angka!", '1;31'))
             sleep(2)
 
 if __name__ == "__main__":
